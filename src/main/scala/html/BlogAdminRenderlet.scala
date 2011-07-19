e384f722bc74e857a56fa94b3c6a2aec73b7d4da
Reto Bachmann-Gmür <reto@apache.org>
2011-07-19 15:30:44 +0200

    cleaned up and added blog management page on /blog

---
+++ src/main/scala/html/BlogAdminRenderlet.scala
@@ -0,0 +1,60 @@
+package org.wymiwyg.gradino.html
+
+import org.wymiwyg.gradino.Ontology
+import scala.xml.XML
+import javax.ws.rs.core.MediaType
+import java.util.Date
+import java.text.DateFormat
+import org.apache.clerezza.platform.typerendering._
+import org.apache.clerezza.rdf.core.UriRef
+import org.apache.clerezza.rdf.utils.GraphNode
+import org.apache.clerezza.rdf.core._
+import impl.util.W3CDateFormat
+import org.apache.clerezza.rdf.utils._
+import org.apache.clerezza.rdf.scala.utils.Preamble._
+import org.apache.clerezza.platform.typerendering.scala._
+import org.apache.clerezza.rdf.ontologies._
+import org.xml.sax.SAXParseException
+
+
+/**
+ * A Renderlet for rss:items
+ */
+class BlogAdminRenderlet extends SRenderlet {
+
+	val getRdfType = Ontology.BlogAdminPage
+
+	override def getModePattern = "naked"
+
+	override def renderedPage(arguments: XmlResult.Arguments) = {
+		new XmlResult(arguments) {
+			override def content = {
+				val baseUri = arguments.requestProperties.getUriInfo.getBaseUri.toString
+				<div>
+					<h1>Blog Administration</h1>
+					At the following URIs the most recent posts can be retrieved:
+					{
+						for (lip <- res!!) yield {
+							<div>
+								<form action="/blog/removeLip" method="post">
+									<span>{lip}</span><input type="hidden" name="lip" value={lip*} />
+									<input type="submit" value="remove"/>
+								</form>
+							</div>
+						}
+					}
+					<form action="/blog/addLip" method="post">
+						Add Uri:
+						<input type="text" name="lip" value={baseUri} />
+						<input type="submit" value="Add"/>
+					</form>
+					<p>
+						<a href={baseUri+"blog/newpost?mode=tiny"}>Create new post with tinyMCE wysiwyg editor</a><br/>
+						<a href={baseUri+"blog/newpost?mode=markDown"}>Create new post writing MarkDown enriched text</a><br/>
+					</p>
+				</div>
+			}
+		}
+	}
+
+}
